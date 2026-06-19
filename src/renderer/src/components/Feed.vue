130e933493da2482f8355659062f314b298a2de3
rmcbridetaylor <reginald.j.mcbridetaylor@gmail.com>
2026-06-18 20:25:38 -0400

    Updated to chunked loading to improve performance

--- src/renderer/src/components/Feed.vue
+++ src/renderer/src/components/Feed.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import ImageCard from './ImageCard.vue'
 
 const model = defineModel<File[]>({ default: [] })
 const emit = defineEmits(['rate', 'open'])
+const pageLength = ref(9)
 
 interface File {
   id: number
@@ -13,16 +14,6 @@ interface File {
   rating: number
 }
 
-const collection1 = computed(() => {
-  return model.value.filter((_n, i) => i % 3 == 0)
-})
-const collection2 = computed(() => {
-  return model.value.filter((_n, i) => i % 3 == 1)
-})
-const collection3 = computed(() => {
-  return model.value.filter((_n, i) => i % 3 == 2)
-})
-
 const rate = (rate): void => {
   var formattedRate = { id: rate.id, rating: rate.rating.target.value }
   emit('rate', formattedRate)
@@ -31,33 +22,75 @@ const rate = (rate): void => {
 const open = (fileId): void => {
   emit('open', fileId)
 }
+
+const loadMoreContent = (): void => {
+  pageLength.value = pageLength.value + 9
+}
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      // If the anchor div is visible in the viewport, load data
+      if (entry.isIntersecting) {
+        loadMoreContent()
+      }
+    })
+  },
+  {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.1
+  }
+)
+
+onMounted(() => {
+  const anchor = document.getElementById('loadAnchor')
+  if (anchor) {
+    observer.observe(anchor)
+  }
+})
 </script>
 
 <template>
   <div class="md:w-4/5 mx-auto overflow-y-auto">
-
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6">
       <div>
         <TransitionGroup name="collection1" tag="div" class="flex flex-col gap-4">
-          <div v-for="(file, index) in collection1" :key="index">
-            <ImageCard :file="file" @rate="rate" @open="open" />
-          </div>
+          <template v-for="(file, index) in model" :key="index">
+            <ImageCard
+              v-if="index % 3 == 0 && index <= pageLength"
+              :file="file"
+              @rate="rate"
+              @open="open"
+            />
+          </template>
         </TransitionGroup>
       </div>
       <div>
         <TransitionGroup name="collection1" tag="div" class="flex flex-col gap-4">
-          <div v-for="(file, index) in collection2" :key="index">
-            <ImageCard :file="file" @rate="rate" @open="open"/>
-          </div>
+          <template v-for="(file, index) in model" :key="index">
+            <ImageCard
+              v-if="index % 3 == 1 && index <= pageLength"
+              :file="file"
+              @rate="rate"
+              @open="open"
+            />
+          </template>
         </TransitionGroup>
       </div>
       <div>
         <TransitionGroup name="collection1" tag="div" class="flex flex-col gap-4">
-          <div v-for="(file, index) in collection3" :key="index">
-            <ImageCard :file="file" @rate="rate" @open="open"/>
-          </div>
+          <template v-for="(file, index) in model" :key="index">
+            <ImageCard
+              v-if="index % 3 == 2 && index <= pageLength"
+              :file="file"
+              @rate="rate"
+              @open="open"
+            />
+          </template>
         </TransitionGroup>
       </div>
+      <div id="loadAnchor" />
     </div>
   </div>
 </template>
